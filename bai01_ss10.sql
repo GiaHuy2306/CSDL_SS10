c529ef575809709fc0cf305c8d09cb7539f92aa2
Gia Huy <sieuka1990@gmail.com>
2026-01-11 20:00:46 +0700

    Add files via upload

---
+++ bai01_ss10.sql
@@ -0,0 +1,29 @@
+USE social_network_pro;
+
+CREATE OR REPLACE VIEW view_users_firstname AS
+SELECT
+    user_id,
+    username,
+    full_name,
+    email,
+    created_at
+FROM users
+WHERE full_name LIKE 'Nguyễn%';
+
+SELECT * FROM view_users_firstname;
+
+INSERT INTO users (username, full_name, gender, email, password)
+VALUES (
+    'nguyen_nv_moi',
+    'Nguyễn Văn Nhân Viên',
+    'Nam',
+    'nguyennv@gmail.com',
+    'hashed_password'
+);
+
+SELECT * FROM view_users_firstname;
+
+DELETE FROM users
+WHERE username = 'nguyen_nv_moi';
+
+SELECT * FROM view_users_firstname;
---
+++ bai06_ss10.sql
@@ -0,0 +1,21 @@
+USE social_network_pro;
+
+CREATE OR REPLACE VIEW view_users_summary AS
+SELECT
+    u.user_id,
+    u.username,
+    COUNT(p.post_id) AS total_posts
+FROM users u
+LEFT JOIN posts p
+    ON u.user_id = p.user_id
+GROUP BY
+    u.user_id,
+    u.username;
+
+SELECT
+    user_id,
+    username,
+    total_posts
+FROM view_users_summary
+HAVING total_posts > 5;
+
